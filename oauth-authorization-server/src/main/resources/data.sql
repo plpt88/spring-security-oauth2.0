78b01ab724c5578aeb7c8eca8d346eea550f458f
Plamen Petrov <plamen.petrov@emerchantpay.com>
2018-10-31 18:04:29 +0200

    Initial commit

---
+++ oauth-authorization-server/src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO oauth_client_details
+	(client_id, client_secret, scope, authorized_grant_types,
+	web_server_redirect_uri, authorities, access_token_validity,
+	refresh_token_validity, additional_information, autoapprove)
+VALUES
+	('com.ezeewallet', 'RZVtK98GnF', 'transaction,read,write',
+	'password,authorization_code,refresh_token', null, null, 36000, 36000, null, true);
+INSERT INTO oauth_client_details
+	(client_id, client_secret, scope, authorized_grant_types,
+	web_server_redirect_uri, authorities, access_token_validity,
+	refresh_token_validity, additional_information, autoapprove)
+VALUES
+	('sampleClientId', 'secret', 'transaction,read,write',
+	'implicit', null, null, 36000, 36000, null, false);
+INSERT INTO oauth_client_details
+	(client_id, client_secret, scope, authorized_grant_types,
+	web_server_redirect_uri, authorities, access_token_validity,
+	refresh_token_validity, additional_information, autoapprove)
+VALUES
+	('barClientIdPassword', 'secret', 'transaction,read,write',
+	'password,authorization_code,refresh_token', null, null, 36000, 36000, null, true);
